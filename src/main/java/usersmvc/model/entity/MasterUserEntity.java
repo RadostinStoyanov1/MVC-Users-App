a49284c65090838ce0781fbaf32425157671a2cf
RadostinStoyanov <Radostin_Stoyanov@students.softuni.bg>
2025-01-19 18:33:19 +0200

    Creating of master user

---
+++ src/main/java/usersmvc/model/entity/MasterUserEntity.java
@@ -0,0 +1,93 @@
+package usersmvc.model.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "master_users")
+public class MasterUserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(nullable = false, unique = true)
+    private String username;
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<UserRoleEntity> roles;
+
+    public long getId() {
+        return id;
+    }
+
+    public MasterUserEntity setId(long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public MasterUserEntity setUsername(String username) {
+        this.username = username;
+        return this;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public MasterUserEntity setFirstName(String firstName) {
+        this.firstName = firstName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public MasterUserEntity setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public MasterUserEntity setEmail(String email) {
+        this.email = email;
+        return this;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public MasterUserEntity setPassword(String password) {
+        this.password = password;
+        return this;
+    }
+
+    public List<UserRoleEntity> getRoles() {
+        return roles;
+    }
+
+    public MasterUserEntity setRoles(List<UserRoleEntity> roles) {
+        this.roles = roles;
+        return this;
+    }
+}
---
+++ src/main/java/usersmvc/model/entity/UserRoleEnum.java
@@ -0,0 +1,5 @@
+package usersmvc.model.entity;
+
+public enum UserRoleEnum {
+    ADMIN, USER
+}
