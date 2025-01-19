6ae4715ba6eee9b670b1df16627fcdbc90564b05
RadostinStoyanov <Radostin_Stoyanov@students.softuni.bg>
2025-01-19 16:44:52 +0200

    Creating of project

---
+++ src/test/java/usersmvc/UsersMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package usersmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsersMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
